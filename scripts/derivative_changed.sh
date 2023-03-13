69e7f14d3f9da5bfb8ad90ee6a2dfd5cb38b852f
rickstaa <rick.staa@outlook.com>
2023-03-13 10:38:23 +0100

    feat: remove unused files

--- scripts/derivative_changed.sh
+++
@@ -1,5 +0,0 @@
-# Check if the derivative files have changed.
-main_database_files=("plane-alert-db.csv", "plane-alert-pia.csv", "plane-alert-twitter-blocked.csv", "plane-alert-ukraine.csv", "plane_images.txt")
-
-files_changed=$(gh pr view 99 --json files -q '.files[].path' | grep '.csv')
-if 
