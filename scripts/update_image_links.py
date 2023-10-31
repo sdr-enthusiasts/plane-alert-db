d32dbb5852886bcf721dfc172b7a0f3893f4cf70
Phaeton <808865+Phaeton@users.noreply.github.com>
2023-10-31 14:45:24 -0400

    black format

--- scripts/update_image_links.py
+++ scripts/update_image_links.py
@@ -8,12 +8,12 @@ search_text = "https://cdn.jetphotos.com/400/"
 replace_text = "https://cdn.jetphotos.com/full/"
 
 logging.info("Reading 'plane_images.csv'.")
-with open(r'plane_images.csv','r') as images:
+with open(r"plane_images.csv", "r") as images:
     data = images.read()
     data = data.replace(search_text, replace_text)
 
 logging.info("Replace '/400/' paths with '/full/'.")
-with open(r'plane_images.csv','w') as images:
+with open(r"plane_images.csv", "w") as images:
     images.write(data)
 
 logging.info("Update image links complete.")
