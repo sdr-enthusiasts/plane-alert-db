7c649146a2a4e1d8cf60b6d8d7e636553663a0e8
Phaeton <808865+Phaeton@users.noreply.github.com>
2023-10-31 20:52:32 -0400

    Revert update_image_links.py Reorder create_db_derivatives.yaml

--- scripts/update_image_links.py
+++ scripts/update_image_links.py
@@ -7,24 +7,13 @@ logging.basicConfig(
 search_text = "https://cdn.jetphotos.com/400/"
 replace_text = "https://cdn.jetphotos.com/full/"
 
-files = [
-    "plane_images.csv",
-    "plane-alert-civ-images.csv",
-    "plane-alert-db-images.csv",
-    "plane-alert-gov-images.csv",
-    "plane-alert-mil-images.csv",
-    "plane-alert-pol-images.csv",
-    "plane-alert-ukraine-images.csv"
-]
+logging.info("Reading 'plane_images.csv'.")
+with open(r"plane_images.csv", "r") as images:
+    data = images.read()
+    data = data.replace(search_text, replace_text)
 
-for item in files:
-    logging.info(f"Reading the '{item}' file.")
-    with open(f"{item}", "r") as images:
-        data = images.read()
-        data = data.replace(search_text, replace_text)
+logging.info("Replace '/400/' paths with '/full/'.")
+with open(r"plane_images.csv", "w") as images:
+    images.write(data)
 
-    logging.info("Replace '/400/' paths with '/full/'.")
-    with open(f"{item}", "w") as images:
-        images.write(data)
-
-    logging.info(f"Update '{item}' complete.")
+logging.info("Update image links complete.")
