e0f1b33f015f110ec18fdb0fba12d83b83377adf
Phaeton <808865+Phaeton@users.noreply.github.com>
2023-12-18 16:36:37 -0500

    Update 'update_images_links.py' for better logging and to be more robust for future updates.

--- scripts/update_image_links.py
+++ scripts/update_image_links.py
@@ -6,14 +6,24 @@ logging.basicConfig(
 
 search_text = "https://cdn.jetphotos.com/400/"
 replace_text = "https://cdn.jetphotos.com/full/"
+jpcount = 0
 
 logging.info("Reading 'plane_images.csv'.")
 with open(r"plane_images.csv", "r") as images:
-    data = images.read()
-    data = data.replace(search_text, replace_text)
+    for line in images:
+        jpcount += line.count(search_text)
 
-logging.info("Replace '/400/' paths with '/full/'.")
-with open(r"plane_images.csv", "w") as images:
-    images.write(data)
+if jpcount > 0:
+    logging.info(f"Occurences of /400/ found: {jpcount}")
+    logging.info("Replacing '/400/' paths with '/full/'.")
+    with open(r"plane_images.csv", "r") as images:
+        data = images.read()
+        data = data.replace(search_text, replace_text)
 
-logging.info("Update image links complete.")
+    logging.info("Writing changes...")
+    with open(r"plane_images.csv", "w") as images:
+        images.write(data)
+        logging.info("Update image links complete!")
+
+else:
+    logging.info("No image links to update!")
