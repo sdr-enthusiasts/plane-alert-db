1e14ce1b534462c5a7beae06a6a07bc46bce495d
Phaeton <808865+Phaeton@users.noreply.github.com>
2024-08-14 16:00:23 -0400

    Improve logging, remove runs for image only PRs

--- scripts/export_categories.py
+++ scripts/export_categories.py
@@ -20,7 +20,8 @@ if __name__ == "__main__":
         .reset_index(drop=True)
     )
     logging.info(f"Total Categories Count: ({category_unique_df.shape[0]}).")
-
+    logging.info("Creating the plane-alert-categories.csv file.")
+    
     category_unique_df.to_csv(
     "plane-alert-categories.csv",
     mode="wb",
@@ -29,3 +30,4 @@ if __name__ == "__main__":
     encoding="utf8",
     lineterminator="\n",
     )
+    logging.info("File created successfully!")
