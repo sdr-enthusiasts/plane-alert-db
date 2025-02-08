8f3bc6115f83359039a1bad432ebe098e84f0701
Phaeton <808865+Phaeton@users.noreply.github.com>
2025-02-08 14:06:31 -0500

    linting

--- scripts/export_categories.py
+++ scripts/export_categories.py
@@ -1,4 +1,4 @@
-""""script to export current categories, this time sorted descending by frequency"""
+""" "script to export current categories, sorted descending by frequency"""
 
 import logging
 import pandas as pd
@@ -11,11 +11,13 @@ if __name__ == "__main__":
     logging.info("Reading the main csv file...")
     df = pd.read_csv("plane-alert-db.csv")
 
-    logging.info("Sorting categories by frequency...")
-    category_sorted = df["Category"].value_counts().index.to_series().reset_index(drop=True)
+    logging.info("Identifying and sorting categories by frequency...")
+    category_sorted = (
+        df["Category"].value_counts().index.to_series().reset_index(drop=True)
+    )
 
     logging.info(f"Total Unique Categories: ({category_sorted.shape[0]}).")
-    logging.info("Creating the plane-alert-categories.csv file sorted by frequency.")
+    logging.info("Creating the plane-alert-categories.csv file, sorted by frequency.")
 
     category_sorted.to_csv(
         "plane-alert-categories.csv",
