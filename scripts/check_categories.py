8acda5a2085f67e2b915e8d1ea1164f86de4da9c
noah <61599590+lingawakad@users.noreply.github.com>
2025-02-09 17:26:41 -0500

    couple more (#661)
    * couple more
    
    * fix category check script
    
    * fix whitespace
    
    ---------
    
    Co-authored-by: Phaeton <808865+Phaeton@users.noreply.github.com>

--- scripts/check_categories.py
+++ scripts/check_categories.py
@@ -1,4 +1,4 @@
-""""script to flag new/invalid categories"""
+"""script to flag new/invalid categories"""
 
 import logging
 import pandas as pd
@@ -10,25 +10,30 @@ logging.basicConfig(
 
 if __name__ == "__main__":
     logging.info("Reading the main csv file...")
-    df = pd.read_csv("plane-alert-db.csv")
+    category_unique_df = pd.read_csv("plane-alert-db.csv")
 
     category_unique_df = (
-        df["Category"]
+        category_unique_df["Category"]
         .drop_duplicates()
+        .sort_values()
         .reset_index(drop=False)
+        .drop("index", axis=1)
     )
-    category_unique_df = category_unique_df.drop('index', axis=1)
+
     logging.info(f"Total Categories in PR Count: ({category_unique_df.shape[0]}).")
 
     logging.info("Reading the export category csv file...")
     valid_df = pd.read_csv("plane-alert-categories.csv")
+    valid_df = (
+        valid_df["Category"].sort_values().reset_index(drop=False).drop("index", axis=1)
+    )
 
     if not valid_df.equals(category_unique_df):
         logging.info("Invalid category used!")
-        merged_df = valid_df.merge(category_unique_df, indicator=True, how='outer')
-        changed_df = merged_df[merged_df['_merge'] == 'right_only']
-        changed_df = changed_df.drop('_merge', axis=1)
-        
+        merged_df = valid_df.merge(category_unique_df, indicator=True, how="outer")
+        changed_df = merged_df[merged_df["_merge"] == "right_only"]
+        changed_df = changed_df.drop("_merge", axis=1)
+
         logging.info(
             "New Categories found ({}):\n{}".format(
                 changed_df.shape[0],
@@ -36,8 +41,6 @@ if __name__ == "__main__":
             )
         )
 
-        sys.stdout.write(
-            f"The files contain invalid or new Categories:\n"
-        )
+        sys.stdout.write(f"The files contain invalid or new Categories:\n")
         sys.exit(1)
     logging.info("Categories check good!")
